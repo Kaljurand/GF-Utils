bf627709b4ab2e62c2871e24f194e626b80f40c6
Kaarel Kaljurand <kaljurand@gmail.com>
2012-10-25 17:33:47 +0200

    Add initial set of scripts

---
+++ make-pgf.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+# Compiles a PGF on the GF cloud
+# Author: Kaarel Kaljurand
+# Version: 2012-10-25
+#
+# Examples:
+#
+# python make-pgf.py --dir /tmp/gfse.123 --langs Eng,Ger Phrasebook
+#
+import sys
+import argparse
+import os
+import json
+import subprocess
+import re
+
+curl='curl'
+EXT='.gf'
+
+# Commandline arguments parsing
+parser = argparse.ArgumentParser(description='Compiles a PGF on the GF cloud')
+
+parser.add_argument('name', metavar=('NAME'), type=str,
+	help='name of the grammar (i.e. abstract syntax)')
+
+parser.add_argument('-d', '--dir', type=str, action='store', dest='dir',
+	help='GF webservice directory (OBLIGATORY)')
+
+parser.add_argument('-s', '--server', type=str, action='store', dest='server',
+	default='http://localhost:41296',
+	help='name of the GF cloud service, default=http://localhost:41296')
+
+parser.add_argument('-l', '--langs', type=str, action='store', dest='langs',
+	help='comma-separated list of 3-letter language codes, e.g. Eng,Ger,Spa (OBLIGATORY)')
+
+parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
+
+args = parser.parse_args()
+
+if args.dir is None:
+	print >> sys.stderr, 'ERROR: argument -d/--dir is not specified'
+	exit()
+
+if args.langs is None:
+	print >> sys.stderr, 'ERROR: argument -l/--langs is not specified'
+	exit()
+
+name = args.name
+langs = args.langs.split(',')
+params = list( map( (lambda x: '-d' + name + x + EXT + '='), langs) )
+params.append('-d' + name + EXT + '=')
+cmd = [curl, '-d', "dir=" + args.dir, '-d', 'command=remake']
+cmd.extend(params)
+cmd.append(args.server + "/cloud")
+#print >> sys.stderr, ' '.join(cmd)
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out,err = p.communicate()
+print json.dumps(json.loads(out), indent=4)
---
+++ upload-grammar.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+# Uploads GF grammar modules to the GF cloud.
+# Author: Kaarel Kaljurand
+# Version: 2012-10-25
+#
+# Examples:
+#
+# python upload-grammar.py --dir /tmp/gfse.123 g1 g2
+#
+import sys
+import argparse
+import os
+import subprocess
+import re
+
+curl='curl'
+# We want to upload only gf and gfo files.
+ext_pattern='^\.(gf|gfo)$'
+
+def gf_file_generator(src_dirs):
+	"""
+	Generates relative pathnames that correspond to
+	files with the extension $ext_pattern in the given directories.
+	"""
+	for top in src_dirs:
+		for root, dirs, files in os.walk(top, topdown=False, followlinks=False):
+			for name in files:
+				path = os.path.join(root, name)
+				basename, extension = os.path.splitext(path)
+				if re.match(ext_pattern, extension):
+					yield [root, name]
+
+
+def upload_files(dir, files):
+	for [root, name] in files:
+		print '{:} {:}'.format(name, root)
+		upload(dir, root, name)
+
+
+def upload(dir, root, name):
+	"""
+	Uploads the given files using Curl
+	"""
+	path = os.path.join(root, name)
+	cmd = [curl,
+		'-d', "dir=" + dir,
+		'-d', 'command=upload',
+		'--data-urlencode', name + "@" + path,
+		args.server + "/cloud"]
+	#print >> sys.stderr, ' '.join(cmd)
+	subprocess.call(cmd)
+
+
+# Commandline arguments parsing
+parser = argparse.ArgumentParser(description='Uploads GF files to the GF cloud')
+
+parser.add_argument('src_dirs', metavar='SRC', type=str, nargs='+',
+	help='local directories to be copied to the server')
+
+parser.add_argument('-d', '--dir', type=str, action='store', dest='dir',
+	help='GF webservice directory (OBLIGATORY)')
+
+parser.add_argument('-s', '--server', type=str, action='store', dest='server',
+	default='http://localhost:41296',
+	help='name of the GF cloud service, default=http://localhost:41296')
+
+parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
+
+args = parser.parse_args()
+
+if args.dir is None:
+	print >> sys.stderr, 'ERROR: argument -d/--dir is not specified'
+	print >> sys.stderr, 'You can generate a new directory on the server by: curl ' + args.server + '/new'
+	exit()
+
+g = gf_file_generator(args.src_dirs)
+upload_files(args.dir, g)
