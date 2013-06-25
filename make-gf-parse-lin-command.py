71825d6b74ef60371800b2087027b38bec2bf1eb
Kaarel Kaljurand <kaljurand@gmail.com>
2013-06-25 09:31:44 +0200

    Improve make-gf-parse-lin-command.py

--- make-gf-parse-lin-command.py
+++ make-gf-parse-lin-command.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Author: Kaarel Kaljurand
-# Version: 2013-06-18
+# Version: 2013-06-25
 #
 # Example:
 #   $ echo "hallo" | python make_gf_parse_lin_command.py --lang=Ger --cat=Phrase
@@ -9,46 +9,54 @@
 
 import sys
 import argparse
+import re
 
-def process_lines(lang_in, lang_out, cat):
+pattern = re.compile(r'\s+')
+
+def process_lines(lang_in, lang_out, cat, linearize_all):
+	flag_cat = ""
+	flag_linearize_all = ""
+	if cat is not None:
+		flag_cat = "-cat=" + cat
+	if linearize_all:
+		flag_linearize_all = "-all"
 	for line in sys.stdin:
-		line = line.strip()
-		process_line(lang_in, lang_out, cat, line)
-
-
-def process_line(lang_in, lang_out, cat, line):
-	serialize_command(lang_in, lang_out, cat, line)
-
+		serialize_command(rewrite_lang(lang_in), rewrite_lang(lang_out), flag_cat, flag_linearize_all, line.strip())
 
 # TODO: add escaping
-def serialize_command(lang_in, lang_out, cat, line):
-	print 'p -lang={:} -cat={:} "{:}" | l -lang={:} -treebank -all'.format(lang_in, cat, line, lang_out)
+def serialize_command(lang_in, lang_out, flag_cat, flag_linearize_all, line):
+	cmd = 'p %s %s "%s" | l %s %s -treebank' % (lang_in, flag_cat, line, lang_out, flag_linearize_all)
+	print re.sub(pattern, ' ', cmd)
+
+def rewrite_lang(lang):
+	if lang is None:
+		return ""
+	return "-lang=" + lang.replace(' ', ',')
 
 
-parser = argparse.ArgumentParser(description='Makes a GF parse + linearize command')
+parser = argparse.ArgumentParser(
+	description='Makes a GF parse + linearize command',
+	epilog="That's all it does."
+)
 
 parser.add_argument('-l', '--lang-in',
 	type=str, action='store', dest='lang_in',
-	default='Eng',
-	help='3-letter language code, e.g. Ger (defaults to Eng)')
+	help='3-letter language code, e.g. Eng')
 
 parser.add_argument('--lang-out',
 	type=str, action='store', dest='lang_out',
-	default='Eng',
-	help='3-letter language code, e.g. Ger (defaults to Eng)')
+	help='3-letter language code (or a sequence of codes), e.g. Ger,Spa')
 
 parser.add_argument('-c', '--cat',
 	type=str, action='store', dest='cat',
-	default='Phrase',
-	help='start category (defaults to Phrase)')
+	help='start category')
+
+parser.add_argument("--all",
+	action="store_true", dest="linearize_all", default=False,
+	help="linearize all variants")
 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s v0.1')
 
 args = parser.parse_args()
 
-if args.lang_out is None:
-	args.lang_out = ""
-else:
-	args.lang_out = args.lang_out.replace(' ', ',')
-
-process_lines(args.lang_in, args.lang_out, args.cat)
+process_lines(args.lang_in, args.lang_out, args.cat, args.linearize_all)
